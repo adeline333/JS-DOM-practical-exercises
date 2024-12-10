e955e22aaaf1cd5b36eb203d21d6f47e1f0ad739
Gym Umurage II <gymumurageii@Umurages-iMac.local>
2024-12-10 17:41:21 +0200

    created files for the to do app in the DOM exercises starting with the full HTML

---
+++ script.js
@@ -0,0 +1,18 @@
+let button =document.getElementById('addButton');
+let inputField = document.getElementById('todoInput');
+let todoList = document.getElementById('todoList');
+
+button.addEventListener('click', function (){
+
+    let enteredText= inputField.value;
+    if (enteredText!=""){
+        let newItem= document.createElement('li');
+        newItem.textContent=enteredText;
+        todoList.appendChild(newItem);
+    }
+
+
+})
+
+
+
