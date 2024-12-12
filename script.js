9141543449b4532c6502faa4c2b7858495dd9785
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2024-12-12 22:45:31 +0200

    updated the script by enabling the user to remive an item from the list and also added some comments in the HTML for better understanding

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ let todoList = document.getElementById('todoList');
 
 button.addEventListener('click', function (){
 
-    let enteredText= inputField.value;
+    let enteredText= inputField.value; //.value to get the text the user typed in the input field
     if (enteredText!=""){
         let newItem= document.createElement('li');
         newItem.textContent=enteredText;
         todoList.appendChild(newItem);
+
+        let removeButton=document.createElement('button');
+       removeButton.textContent='Remove';
+        removeButton.setAttribute('style','padding:5px 5px;');
+        newItem.appendChild(removeButton);
+        removeButton.addEventListener('click',function(){
+            newItem.remove();
+
+            let checkBox=document.createElement('checkbox');
+            todoList.appendChild(checkBox);
+        })
     }
 
 else{
